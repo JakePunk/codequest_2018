4a55a40fcfcc70e2ceebccf5a1b1383b479ddd66
Jacob Lynn <jacoblynn@danlynn-3.local>
2019-02-12 17:59:01 -0500

    Initial commit

---
+++ pass_fail/pass_fail.py
@@ -0,0 +1,10 @@
+# pass/fail algorithm
+
+with open("input.txt", 'r') as filehandle:
+    count = int(filehandle.readline())
+    for x in range(count):
+        line = int(filehandle.readline())
+        if line < 70:
+            print ("FAIL")
+        else:
+            print("PASS")
